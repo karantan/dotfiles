99205fcc4b837173a6b383040ffee23f93f8577f
Gasper Vozel <gv@niteo.co>
2025-04-24 11:50:58 +0200

    Add git config

--- flake.nix
+++ flake.nix
@@ -48,6 +48,58 @@
         enableZshIntegration = true;
       };
 
+      programs.git = {
+        enable = true;
+        userName = "Gasper Vozel";
+        userEmail = secrets.email;
+        extraConfig = {
+          core = {
+            editor = "code --wait";
+          };
+          diff = {
+            tool = "diffmerge";
+          };
+          github = {
+            user = "karantan";
+            token = secrets.github.token;
+          };
+        };
+        ignores = [
+          # Packages: it's better to unpack these files and commit the raw source
+          # git has its own built in compression methods
+          "*.7z"
+          "*.dmg"
+          "*.gz"
+          "*.iso"
+          "*.jar"
+          "*.rar"
+          "*.tar"
+          "*.zip"
+
+          # OS generated files
+          ".DS_Store"
+          ".DS_Store?"
+          "ehthumbs.db"
+          "Icon?"
+          "Thumbs.db"
+
+          # Sublime
+          "sublime/*.cache"
+          "sublime/oscrypto-ca-bundle.crt"
+          "sublime/Package Control.last-run"
+          "sublime/Package Control.merged-ca-bundle"
+          "sublime/Package Control.user-ca-bundle"
+
+          # VS Code
+          "vscode/History/"
+          "vscode/globalStorage/"
+          "vscode/workspaceStorage/"
+
+          # Secrets
+          "ssh_config_private"
+        ];
+      };
+
       programs.zsh = {
         enable = true;
         autosuggestion.enable = true;
